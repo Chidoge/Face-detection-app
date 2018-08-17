642df9fb8034f6173145695755662f55fe4d1d42
Lincoln Choy <lincoln.choy1997@gmail.com>
2018-08-18 00:15:29 +1200

    Face detection API is called when link is entered and button is pressed

--- face_recognition/src/App.js
+++ face_recognition/src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Clarifai from 'clarifai';
 import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
 import './App.css';
+
+const app = new Clarifai.App({
+	apiKey : '4f96d4e2d08446aaa088f3fd8cb83798'
+});
 
 const particlesOptions = {
  	particles: {
@@ -20,6 +26,36 @@ const particlesOptions = {
 
 /* The main container for the page */
 class App extends Component {
+
+	constructor() {
+
+		super();
+
+		this.state = {
+			input : '',
+			imageUrl : ''
+		}
+	}
+
+	onInputChange = (event) => {
+		this.setState({ input : event.target.value });
+	}
+
+	onButtonSubmit = () => {
+
+		this.setState({ imageUrl : this.state.input });
+		app.models.predict(Clarifai.FACE_DETECT_MODEL,this.state.input).then(
+			function(response) {
+				console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+
+			},
+			function(err) {
+				// there was an error
+				console.log(err);
+			}
+		);
+	}
+
 	render() {
 		return (
 			<div className = 'App'>
@@ -27,7 +63,8 @@ class App extends Component {
 				<Navigation />
 				<Logo />
 				<Rank />
-				<ImageLinkForm />
+				<ImageLinkForm onButtonSubmit = {this.onButtonSubmit} onInputChange = {this.onInputChange} />
+				<FaceRecognition imageUrl = {this.state.imageUrl} />
 			</div>
 		);
 	}
