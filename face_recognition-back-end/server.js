5861b741cc81eb16804969daab21c843ea42cb11
Lincoln Choy <lincoln.choy1997@gmail.com>
2018-08-22 09:59:54 +1200

    Minor comment changes

--- face_recognition-back-end/server.js
+++ face_recognition-back-end/server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 
-
 const app = express();
+
+app.get('/', (req,res) => {
+	res.send('this is working');
+})
 
 app.listen(3000, () => {
 	console.log("Server started");
 });
+
+
+
+
+/*
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userID --> GET = user
+/image --> PUT = user
+
+*/
