9a143ee7eba79584419daef9754296f8c29a63a2
Lincoln Choy <lincoln.choy1997@gmail.com>
2018-08-26 15:40:21 +1200

    App has all API's implemented using postgres

--- face_recognition-back-end/server.js
+++ face_recognition-back-end/server.js
@@ -19,10 +19,6 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-db.select('*').from('users').then(data => {
-	console.log(data);
-});
-
 
 app.get('/', (req,res) => {
 
@@ -32,32 +28,66 @@ app.get('/', (req,res) => {
 
 app.post('/signin', (req,res) => {
 
+	const { email, password } = req.body;
 
-	if (req.body.email === db.users[0].email && 
-		req.body.password === db.users[0].password ) {
-		res.json(db.users[0]);
-	}
-	else {
-		res.status(400).json('fail');
-	}
+	db.select('email','hash').from('login')
+	.where('email','=', email)
+	.then(data => {
+
+		const isValid = bcrypt.compareSync(password,data[0].hash);
+		if (isValid) {
+			return db.select('*').from('users')
+			.where('email','=',email)
+			.then(user => {
+				res.json(user[0]);
+			})
+			.catch(err => {
+				res.status(400).json('Unable to get user');
+			})
+		}
+		else {
+			res.status(400).json('wrongPW');
+		}
+	})
+	.catch(err => {
+		res.status(400).json('Wrong credentials');
+	})
 })
 
 
 app.post('/register', (req,res) => {
 
+	/* Destructure request.body object */
 	const { email, name, password } = req.body;
 
-	db('users')
-	.returning('*')
-	.insert({ 
-		email : email,
-		name : name,
-		joined : new Date()
-	})
-	.then(user => {
-		res.json(user[0]);
+	/* Synchronous hashing */
+	const hash = bcrypt.hashSync(password);
+
+	/* Transaction for consistency */
+	db.transaction(trx => {
+		trx.insert({
+			hash : hash,
+			email : email
+		})
+		.into('login')
+		.returning('email')
+		.then(loginEmail => {
+			return trx('users')
+			.returning('*')
+			.insert({ 
+				email : loginEmail[0],
+				name : name,
+				joined : new Date()
+			})
+			.then(user => {
+				res.json(user[0]);
+			})
+		})
+		.then(trx.commit)
+		.catch(trx.rollback)
 	})
 	.catch(err => res.status(400).json('Unable to register'));
+		
 })
 
 
